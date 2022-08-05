2022e2394066ac94d10634ef6afdf926e299cb50
Mari <git@cutegirl.tech>
2022-08-05 13:52:33 +0200

    Generate cargo crate with dependencies

--- src/generator.rs
+++ src/generator.rs
@@ -1,22 +1,109 @@
-use std::fs::File;
-use std::io::{BufWriter, Write};
-use std::path::Path;
-use anyhow::{Context, Result};
-use okapi::Map;
-use okapi::openapi3::{Components, OpenApi, PathItem};
 use crate::models::generate_models;
+use crate::routes::generate_routes;
+use anyhow::{anyhow, bail, Context, Result};
+use cargo_toml::{Dependency, DependencyDetail, DepsSet, Manifest};
+use okapi::openapi3::OpenApi;
+use std::fs;
+use std::fs::{File, OpenOptions};
+use std::io::Write;
+use std::path::Path;
+use std::process::Command;
 
 pub(crate) fn generate_crate(document: OpenApi, path: &Path) -> Result<()> {
-    // Required dependencies: serde, serde_json, anyhow
-    // TODO WHAT HTTP CLIENT`i'd say isacc https://www.arewewebyet.org/topics/http-clients/
+    let status = Command::new("cargo")
+        .arg("new")
+        .arg("--quiet")
+        .args(["--vcs", "none"])
+        .arg("--lib")
+        .args(["--edition", "2021"])
+        .arg(
+            path.to_str()
+                .ok_or_else(|| anyhow!("Failed to convert output OsStr into &str"))?,
+        )
+        .status()
+        .context("Failed to run Cargo")?;
 
-    if let Some(components) = document.components {
-        generate_models(&components)?;
+    if !status.success() {
+        if let Some(code) = status.code() {
+            bail!("Cargo exited with non-zero status code: {}", code);
+        } else {
+            bail!("Cargo subprocess was killed by another process or system");
+        }
     }
 
-    generate_routes(&document.paths)
+    edit_cargo_toml(path)?;
+
+    let src_directory = path.join("src");
+
+    if let Some(components) = document.components {
+        generate_models(&components, &src_directory)?;
+    }
+
+    generate_routes(&document.paths, &src_directory)
 }
 
-pub(crate) fn generate_routes(paths: &Map<String, PathItem>) -> Result<()> {
-    unimplemented!()
+fn open_manifest(path: &Path) -> Result<Manifest> {
+    Ok(Manifest::from_path(path).context("Failed to parse `Cargo.toml`")?)
+}
+
+fn edit_cargo_toml(path: &Path) -> Result<()> {
+    let path = path.join("Cargo.toml");
+
+    // https://stackoverflow.com/a/50691004/11494565
+    let mut file = OpenOptions::new()
+        .read(false)
+        .write(true)
+        .create(true)
+        .append(false)
+        .open(&path)
+        .context("Failed to open or create `Cargo.toml`")?;
+
+    let mut manifest = open_manifest(&path)?;
+    add_dependencies(&mut manifest.dependencies)?;
+
+    let serialized = toml::to_string(&manifest).context("Failed to serialize `Cargo.toml`")?;
+    file.write_all(serialized.as_bytes())
+        .context("Failed to write `Cargo.toml`")?;
+
+    Ok(())
+}
+
+fn add_dependencies(dependencies: &mut DepsSet) -> Result<()> {
+    // serde
+    {
+        let mut dependency = DependencyDetail::default();
+        dependency.version = Some("1".to_string());
+        dependency.features.push("derive".to_string());
+
+        dependencies.insert("serde".to_string(), Dependency::Detailed(dependency));
+    }
+
+    // serde_json
+    dependencies.insert(
+        "serde_json".to_string(),
+        Dependency::Simple("1".to_string()),
+    );
+
+    // anyhow
+    dependencies.insert("anyhow".to_string(), Dependency::Simple("1".to_string()));
+
+    // surf
+    {
+        let mut dependency = DependencyDetail::default();
+        dependency.version = Some("2".to_string());
+        dependency.features.push("hyper-client".to_string());
+
+        dependencies.insert("surf".to_string(), Dependency::Detailed(dependency));
+    }
+
+    // base64uuid
+    {
+        let mut dependency = DependencyDetail::default();
+        dependency.git = Some("ssh://git@github.com/fiberplane/fiberplane-rs.git".to_string());
+        dependency.branch = Some("main".to_string());
+
+        dependencies.insert("base64uuid".to_string(), Dependency::Detailed(dependency));
+    }
+
+    Ok(())
 }
