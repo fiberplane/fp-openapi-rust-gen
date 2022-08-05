da1e0cc84f685b32f626e708817b0f166a6814f0
Mari <git@cutegirl.tech>
2022-08-05 11:33:37 +0200

    I hate Windows I hate Windows

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+use std::fs;
 use std::fs::File;
 use std::io::BufReader;
 use anyhow::{bail, Context, Result};
 use clap::Parser;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use okapi::openapi3::OpenApi;
 
 mod generator;
@@ -31,8 +32,18 @@ fn main() -> Result<()> {
 
     let output = args.output.as_path();
 
-    if output.exists() && !args.force {
-        bail!("Output crate already exists. Supply --force to allow overwriting");
+    if output.exists() {
+        if args.force {
+            // Windows does not delete the directory until the last handle to it is closed: https://docs.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-removedirectorya#remarks
+            // A handle gets created by `clap` to it however (as its passed as a PathBuf in CLI args)
+            // To work around this, rename the old directory and delete that. I hate Windows.
+            let new_name = Path::new("__tmp_old_crate");
+
+            fs::rename(output, new_name).context("Failed to rename previous output crate")?;
+            fs::remove_dir_all(new_name).context("Failed to delete previous output crate")?;
+        } else {
+            bail!("Output crate already exists. Supply --force to allow overwriting");
+        }
     }
 
     generator::generate_crate(document, output)
